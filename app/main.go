4f0dc3a72cddbc2621b371b97677e376f017bce2
Mark Douthwaite <mark@douthwaite.io>
2020-03-05 22:12:35 +0000

    Added webapp components.

--- app/main.go
+++ app/main.go
@@ -3,15 +3,51 @@ package main
 import (
   "log"
   "net/http"
+  "html/template"
+  "os"
+  // "bytes"
+
+  "github.com/gorilla/mux"
 )
+
+const HOST = ""
+
+type Page struct {
+  Title string
+  Variables string
+}
+
+
+
+func getVariables() (string){
+  // payload := []byte(``)
+  // resp, _ := http.Post("http://localhost:4504/inference/query", "application/json", bytes.NewBuffer(payload))
+
+  return "test"
+}
+
+
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+  w.WriteHeader(http.StatusOK)
+
+  wd, _ := os.Getwd()
+
+  page := Page{Title: "Home", Variables: getVariables()}
+  path := wd + "/templates/index.html"
+
+  t, _ := template.ParseFiles(path)
+  t.Execute(w, page)
+}
+
 
 
 func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/app", HomeHandler)
 
-  http.Handle("/app", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	  log.Println("Hello World!")
-	}))
-
+  http.Handle("/", r)
   log.Println("Webapp live on 3000.")
   http.ListenAndServe(":3000", nil)
 
--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ from apogee.server import ApogeeServer
 
 def run(port=8080, host="127.0.0.1"):
     model = load_model("alarm")
-    server = ApogeeServer(model, port=port, host=host)
+    server = ApogeeServer(model, port=port, host=host, subpath="inference")
     server.run()
 
 
